6285f3a7cf5f302411136ed750e84d80ad445bdf
Nikita Shpikin <nikita.shpikin@gmail.com>
2023-02-10 18:03:43 +0500

    рализовал бурге меню js

--- main.js
+++ main.js
@@ -9,3 +9,21 @@ headerBurger.addEventListener('click', () => {
 		headerMenu.style.opacity = '1';
 	}
 });
+
+let headerLinks = document.querySelectorAll('.header__link');
+
+headerLinks.forEach((link, index) => {
+	link.addEventListener('click', function () {
+		styleLinks(index);
+	});
+});
+
+function styleLinks(index) {
+	console.log(index);
+
+	headerLinks.forEach(item => {
+		item.classList.remove('header__link_active');
+	});
+
+	headerLinks[index].classList.add('header__link_active');
+}
